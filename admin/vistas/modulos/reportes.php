4b32b59ef620a6fb7a5d3d5db5c620cd9133f75d
AlexEscalanteONE <39635523+alexescalante1@users.noreply.github.com>
2021-09-23 00:04:15 -0500

    correcciones 22-09-21
    correcciones graficas y limpieza

--- admin/vistas/modulos/reportes.php
+++ admin/vistas/modulos/reportes.php
@@ -3,9 +3,6 @@
 require_once "../../controladores/reportes.controlador.php";
 require_once "../../modelos/reportes.modelo.php";
 
-require_once "../../controladores/productos.controlador.php";
-require_once "../../modelos/productos.modelo.php";
-
 require_once "../../controladores/usuarios.controlador.php";
 require_once "../../modelos/usuarios.modelo.php";
 
